36df2f60c209d46ac74d1046f7c087ffc5197ddd
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-06-20 10:02:43 +0530

    Create todo.js to handle todos

---
+++ src/todo.js
@@ -0,0 +1,62 @@
+function createTodo(title) {
+  let _id = crypto.randomUUID();
+  let _title = title;
+  let _description = "";
+  let _dueDate = null;
+  let _priority = "normal";
+  let _notes = "";
+  let _isComplete = false;
+
+  const getId = () => _id;
+
+  const getTitle = () => _title;
+
+  const getDescription = () => _description;
+
+  const getDueDate = () => _dueDate;
+
+  const getPriority = () => _priority;
+
+  const getNotes = () => _notes;
+
+  const getIsComplete = () => _isComplete;
+
+  const setTitle = (title) => {
+    _title = title;
+  };
+
+  const setDescription = (description) => {
+    _description = description;
+  };
+
+  //   const setDueDate = (dueDate) => {
+  //     _dueDate = dueDate;
+  //   }
+
+  const setPriority = (priority) => {
+    _priority = priority;
+  };
+
+  const setNotes = (notes) => {
+    _notes = notes;
+  };
+
+  const toggleComplete = () => {
+    _isComplete = _isComplete ? false : true;
+  };
+
+  return {
+    getId,
+    getTitle,
+    getDescription,
+    getDueDate,
+    getPriority,
+    getNotes,
+    getIsComplete,
+    setTitle,
+    setDescription,
+    setPriority,
+    setNotes,
+    toggleComplete,
+  };
+}
