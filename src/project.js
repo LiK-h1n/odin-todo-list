dbb8f48b58735428999872cead2303f046368a70
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-06-20 10:28:06 +0530

    Create project.js to handle projects

---
+++ src/project.js
@@ -0,0 +1,35 @@
+function createProject(title) {
+  let _id = crypto.randomUUID();
+  let _title = title;
+  let _todos = [];
+
+  const getId = () => _id;
+
+  const getTitle = () => _title;
+
+  const setTitle = (title) => {
+    _title = title;
+  };
+
+  const addTodo = (todo) => {
+    _todos.push(todo);
+  };
+
+  const removeTodo = (todoId) => {
+    _todos = _todos.filter((todo) => todo.getId() != todoId);
+  };
+
+  const getTodos = () => _todos;
+
+  const getTodoById = (todoId) => _todos.find((todo) => todo.getId() == todoId);
+
+  return {
+    getId,
+    getTitle,
+    setTitle,
+    addTodo,
+    removeTodo,
+    getTodos,
+    getTodoById,
+  };
+}
