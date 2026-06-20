ecbdac2f6d873b6130af7127230a489965a0830a
LiK-h1n <nikhil.23.idiculla@gmail.com>
2026-06-20 12:01:00 +0530

    Add test data in order to check

--- src/index.js
+++ src/index.js
@@ -16,7 +16,25 @@
 
 // ------------------------------------------------------
 
-import "./styles.css";
+import { projectManager } from "./project-manager.js";
+import { createTodo } from "./todo.js";
 import { displayController } from "./display-controller.js";
+import "./styles.css";
+
+const inbox = projectManager.getInbox();
+inbox.addTodo(createTodo("Finish the Odin Todo project"));
+inbox.addTodo(createTodo("Call the gym for membership details"));
+
+const gymProject = projectManager.addProject("Gym");
+gymProject.addTodo(createTodo("Squats: 3 sets of 10"));
+gymProject.addTodo(createTodo("Deadlifts: 1 rep max"));
+
+const codingProject = projectManager.addProject("Study Plan");
+codingProject.addTodo(createTodo("Master localStorage logic"));
+codingProject.addTodo(createTodo("Learn date-fns formatting"));
+
+const sampleTodo = codingProject.getTodos()[0];
+sampleTodo.setDescription("Crucial for the 20-day sprint goal!");
+sampleTodo.setPriority("high");
 
 displayController.init();
